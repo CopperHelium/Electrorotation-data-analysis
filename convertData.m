79ddc3e66a3c197128465286f89d5c2b34605968
Navish Wadhwa <wadhwa.navish@gmail.com>
2016-12-05 17:52:09 -0500

    Updated the comments in the convertData.m file

--- convertData.m
+++ convertData.m
@@ -3,17 +3,28 @@
 %
 %----------------------------------------------------------------
 %Navish Wadhwa - v1.5
-%This area contians are the parameter which may be changed every times
+
+%This area contians the path to the folder containing the tdms files to be
+%converted. This should be changed everytime new files are to be converted.
+
 path='E:\work\08-04\';
 
 %----------------------------------------------------------------
+
+% Collect a list of all the files in the current directory
 Files=dir(strcat(path,'*.tdms'));
+
+% Measure how long the list of files is
 lengthFiles=length(Files);
+
+%Use simpleConvertTDMS function to convert all files into mat files one
+%after the other. All mat files will be saves in the same directory as the
+%tdms files
 for i=1:lengthFiles
-   % fopen(strcat('E:\work\',Files(i).name),'rt');
-    simpleConvertTDMS(strcat(path,Files(i).name));
+       simpleConvertTDMS(strcat(path,Files(i).name));
 end
 
+% Display completion message when done
 fprintf('Completed''\n');
 clear;
 
