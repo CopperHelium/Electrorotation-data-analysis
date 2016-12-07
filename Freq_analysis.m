e9e3ff24f11a902274285a83dc979b2c8e4ad38b
Navish Wadhwa <wadhwa.navish@gmail.com>
2016-12-07 11:06:38 -0500

    started a new code to do freq data analysis

---
+++ Freq_analysis.m
@@ -0,0 +1,4 @@
+rate = 10000; % number of data recorded per second
+F_state = UntitledVoltage_0.Data; % Shows whether the field was ON or OFF
+X_signal = UntitledVoltage_1.Data; % Contains X-Photomultiplier signal
+Y_signal = UntitledVoltage_2.Data; % Contains Y-Photomultiplier signal
--- README.md
+++
@@ -1,2 +0,0 @@
-# Electrorotation-data-analysis
-This repository contains the matlab codes for analyzing the data generated in electrorotation experiments
